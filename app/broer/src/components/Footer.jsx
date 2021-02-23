3d2da7a350e376671ba66040f46cf510a5e14b52
amkh <ammarkhalil08@gmail.com>
2021-02-23 18:01:25 +0100

    added header component

--- app/broer/src/components/Footer.jsx
+++ app/broer/src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './footer.css'
 
+
 export const Footer = () => {
 
     return (
 
         <div className="footer-main-div">
-            <h1>Hi footer</h1>
+            <div className="icons-container">
+            <img className="insta-icon" src={process.env.PUBLIC_URL + 'Insta.png'} alt ="logo" /> 
+            <img className="in-icon" src={process.env.PUBLIC_URL + 'IN.png'} alt ="logo" /> 
+            <img className="fb-icon" src={process.env.PUBLIC_URL + 'FB.png'} alt ="logo" /> 
+            </div>
         </div>
     );
 }
--- app/broer/src/components/Header.jsx
+++ app/broer/src/components/Header.jsx
@@ -6,7 +6,15 @@ import './header.css'
     return (
 
         <div className="header-main-div">
-            <h1>Hi header</h1>
+            <div className="logo-container">
+            <img className="logo" src={process.env.PUBLIC_URL + 'Logo.png'} alt ="logo" /> 
+            </div>
+
+            <div className="nav-links-container">
+                <p className="nav-link nav-link-ltr">Forside</p>
+                <p className="nav-link nav-link-ltr">Om oss</p>
+                <p className="nav-link nav-link-ltr">Kontakt oss</p>
+            </div>
         </div>
     );
 }
