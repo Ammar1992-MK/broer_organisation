45ecf2f592825348fcce31775444f46a40418327
amkh <ammarkhalil08@gmail.com>
2021-03-05 18:29:57 +0100

    errors with emailjs library to send emails

--- app/broer/src/components/ContactUs/ContactUs.jsx
+++ app/broer/src/components/ContactUs/ContactUs.jsx
@@ -1,37 +1,30 @@
 import React from 'react'
 import {Icons} from '../Icons/Icons'
 import './contactUs.css'
+import emailjs from 'emailjs-com';
 
 export const ContactUs = () => {
 
-    const sendEmail = () => {
+   
 
-        console.log("email");
+    function sendEmail (e) {
+        
+            e.preventDefault();
+        
+            emailjs.sendForm('service_2pj4iyd', 'template_a15hxgn', e.target, 'user_a4aLerBsV4MpRFezikB37')
+              .then((result) => {
+                  console.log(result.text);
+              }, (error) => {
+                  console.log(error.text);
+              });
+
+              e.target.reset();
     }
 
     return (
 
         <div className="contactUs-main-container">
-            <div className="contactus-header">
-                <h1>Ta kontakt !</h1>
-                <p>Vi er åpne for ethvert forslag eller bare å chatte</p>
-
-                <div className="contact-info-container">
-                    <div className="contact-info">
-                        <Icons  type="phone"/>
-                        <p className="contact-info-text">+47 970 09 897</p>
-                    </div>
-                    <div className="contact-info">
-                        <Icons  type="mail"/>
-                        <p className="contact-info-text">broer.org@gmail.com</p>
-                    </div>
-                    <div className="contact-info">
-                        <Icons  type="position"/>
-                        <p className="contact-info-text">Nils Huus' gate 11 0482 OSLO</p>
-                    </div>
-                </div>
-            </div>
-
+            
             <div className="become-memeber-container">
 
                 <form className="from" onSubmit={sendEmail}>
@@ -43,7 +36,9 @@ export const ContactUs = () => {
                     <input className="input" name="number" ></input>
                     <label>Epost</label>
                     <input type="email" className="input" name="email" ></input>
-                    <textarea className="text-message" placeholder="Din melding til oss"></textarea>
+                    <textarea className="text-message" placeholder="Din melding til oss" name="message"></textarea>
+
+                    <input className="submit-btn" type="submit" value="Send"></input>
                 </form>
 
             
--- app/broer/src/components/Footer.jsx
+++ app/broer/src/components/Footer.jsx
@@ -23,6 +23,28 @@ export const Footer = () => {
             <Icons className="socialmedia-icon" type="instagram" />
             <Icons className="socialmedia-icon" type="linkedin"/>
             </div>
+            {/*<div className="contactus-header">
+                <div className="contact-us-text">
+                <h1>Ta kontakt !</h1>
+                <p>Vi er åpne for ethvert forslag eller bare å chatte</p>
+                </div>
+
+                <div className="contact-info-container">
+                    <div className="contact-info">
+                        <Icons  type="phone"/>
+                        <p className="contact-info-text">+47 970 09 897</p>
+                    </div>
+                    <div className="contact-info">
+                        <Icons  type="mail"/>
+                        <p className="contact-info-text">broer.org@gmail.com</p>
+                    </div>
+                    <div className="contact-info">
+                        <Icons  type="position"/>
+                        <p className="contact-info-text">Nils Huus' gate 11 0482 OSLO</p>
+                    </div>
+                </div>
+    </div>*/}
+
         </div>
     );
 }
