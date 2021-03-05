af08bb50c7ce39002549096f713ca714d22dae1f
amkh <ammarkhalil08@gmail.com>
2021-03-05 02:16:53 +0100

    CSS problems!

--- app/broer/src/App.js
+++ app/broer/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from './components/Header'
 import {Footer} from './components/Footer'
 import {HomePage} from './components/Homepage/HomePage'
+import {ContactUs} from './components/ContactUs/ContactUs'
 
 
 function App() {
@@ -12,8 +13,7 @@ function App() {
     <div className="App">
       
       <Header/>
-      <HomePage/>
-      <Footer/> 
+      <ContactUs/>
     </div>
   );
 }
